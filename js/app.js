c82d697ede7df786a8f91b66a4aaf38cbedfaf3c
Danilo <danilofelipe2607@gmail.com>
2021-08-27 10:33:04 -0300

    aula 3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-import { valida } from "./validacao.js";
+import { valida } from './validacao.js'
 
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll('input')
 
-inputs.forEach((input) => {
-  input.addEventListener("blur", (e) => valida(e.target));
-});
+inputs.forEach(input => {
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+    })
+})
